6ce960b14cd5b85ed0b4055b82980fa9ab8a070d
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-09-11 00:13:42 -0300

    Movendo para o diretório correto.

--- src/ExemploAnaliseImagem/Program.cs
+++ src/ExemploAnaliseImagem/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
 using System.IO;
 using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models;
 
-namespace ExemploCustomView
+namespace ExemploAnalise
 {
     class Program
     {
@@ -17,22 +17,28 @@ namespace ExemploCustomView
         static void EfetuarTesteImagem(string caminho, bool eURL)
         {
 
-            PredictionEndpoint endpoint = new PredictionEndpoint() { ApiKey = "API KEY" };
+            string chaveAPI = "0c91a890892d40a0ba9f9e22a5e358de";
+            string chaveProjeto = "4a1a0670-7689-4d01-ac59-fc56227cc3ed";
+
+            // Crio o client do Custom View Service.
+            PredictionEndpoint endpoint = new PredictionEndpoint() { ApiKey = chaveAPI };
             ImagePrediction resultado = null;
 
+            // Verifico se o projeto é uma URL ou um arquivo local para usar o método correto.
             if (eURL)
             {
-                resultado = endpoint.PredictImageUrl(new Guid("Id do Projeto"), new ImageUrl(caminho), new Guid("Id Interação"));
+                resultado = endpoint.PredictImageUrl(new Guid(chaveProjeto), new ImageUrl(caminho));
             }
             else
             {
-                resultado = endpoint.PredictImage(Guid.NewGuid(), File.OpenRead(caminho), new Guid("36ca5bfe-03fd-44bd-aecd-fb6987504362"));
+                resultado = endpoint.PredictImage(new Guid(chaveProjeto), File.OpenRead(caminho));
             }
 
+            // Percorro as analises.
             foreach (var possibilidade in resultado.Predictions)
             {
                 Console.WriteLine($"Tag: {possibilidade.TagName}");
-                Console.WriteLine($"Possibilidade: {possibilidade.Probability:P1}");                
+                Console.WriteLine($"Possibilidade: {possibilidade.Probability:P1}");
             }
         }
     }
